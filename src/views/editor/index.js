8265a25dd3e149ec9079a64021adedfe69d26f21
avinash-vk <avinash2000vk@gmail.com>
2020-09-19 16:54:58 +0530

    added theme options

--- src/views/editor/index.js
+++ src/views/editor/index.js
@@ -10,6 +10,8 @@ import {
 import {Controlled as CodeMirror} from 'react-codemirror2';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/theme/monokai.css';
 import MODES from './modes';
 
 const useStyles = makeStyles({
@@ -38,6 +40,7 @@ const useStyles = makeStyles({
     border: '2px solid white',
     borderRadius: '10px',
     padding: '10px',
+    margin: '20px',
   },
   label: {
     color: 'white',
@@ -46,9 +49,16 @@ const useStyles = makeStyles({
   LangInput: {
     color: 'white',
   },
+  toolbar: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 const Editor = () => {
+  const [theme, setTheme] = useState('material');
   const [lang, setLang] = useState('Javascript');
   const [code, setCode] = useState(
     `import Greeting from 'greetings.js';\n\nconsole.log("hello there",Greeting);`
@@ -57,23 +67,39 @@ const Editor = () => {
   return (
     <div className={classes.root}>
       <h2 className={classes.heading}>Welcome to editor</h2>
-      <FormControl className={classes.formControl}>
-        <InputLabel className={classes.label}>Language</InputLabel>
-        <Select
-          className={classes.LangInput}
-          id="demo-simple-select"
-          value={lang}
-          onChange={(newVal) => {
-            setLang(newVal.target.value);
-          }}
-        >
-          <MenuItem value={'Javascript'}>Javascript</MenuItem>
-          <MenuItem value={'Python'}>Python</MenuItem>
-          <MenuItem value={'C'}>C</MenuItem>
-          <MenuItem value={'C++'}>C++</MenuItem>
-          <MenuItem value={'Java'}>Java</MenuItem>
-        </Select>
-      </FormControl>
+      <div className="toolbar">
+        <FormControl className={classes.formControl}>
+          <InputLabel className={classes.label}>Language</InputLabel>
+          <Select
+            className={classes.LangInput}
+            id="demo-simple-select"
+            value={lang}
+            onChange={(newVal) => {
+              setLang(newVal.target.value);
+            }}
+          >
+            <MenuItem value={'Javascript'}>Javascript</MenuItem>
+            <MenuItem value={'Python'}>Python</MenuItem>
+            <MenuItem value={'C'}>C</MenuItem>
+            <MenuItem value={'C++'}>C++</MenuItem>
+            <MenuItem value={'Java'}>Java</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <InputLabel className={classes.label}>Theme</InputLabel>
+          <Select
+            className={classes.LangInput}
+            value={theme}
+            onChange={(newVal) => {
+              setTheme(newVal.target.value);
+            }}
+          >
+            <MenuItem value={'material'}>Material</MenuItem>
+            <MenuItem value={'dracula'}>Dracula</MenuItem>
+            <MenuItem value={'monokai'}>Monokai</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
       <div className={`Editor ${classes.Editor}`}>
         <CodeMirror
           className={`code-mirror-container ${classes.codeMirror}`}
@@ -81,7 +107,7 @@ const Editor = () => {
           options={{
             lineNumbers: true,
             mode: MODES.getMode(lang),
-            theme: 'material',
+            theme: theme,
             scrollbarStyle: null,
             indentUnit: 2,
             indentWithTabs: true,
